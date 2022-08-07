467c032826bfaed49d1796685cda65b9866443d2
Zeyad-Sayed-70 <102079450+Zeyad-Sayed-70@users.noreply.github.com>
2022-08-07 21:56:39 +0200

    up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 5000
+
+// default middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+app.listen(port, console.log('server is running on port', port))
